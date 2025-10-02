7bd3a5d769098f80f84e744d87a73a6a012ea402
beckslange <beckslange96@gmail.com>
2025-10-01 20:51:52 -0600

    added in a js plugin

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*let heading = document.querySelector("h1");
+
+heading.innerHTML = "H";
+//we are learning plugins so that we can see the animation for this*/
+
+//google typewriter js plugins, use the github one
+//jquery is not used anymore
+//google typewriter js plugins image gallery
+
+new Typewriter("h1", {
+  strings: "Hello World",
+  /*this can be a string "", or an array []. If it is a string, it will not erase
+  after writing. If it is an array, it will write it out and then erase*/
+  autoStart: true,
+  cursor: "",
+  //this makes it so that there is not a line blinking when it is typing/finished typing
+  delay: 60,
+  //this changes the typing speed, 10 is faster than 100
+});
